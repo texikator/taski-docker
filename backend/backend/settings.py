0e35a880bb629547a621b140f710f1c729315112
texikator <wiktot@mail.ru>
2024-06-07 14:19:36 +0300

    WF with flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST',''),
+        'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
