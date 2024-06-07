f0d06c2c7f961052b1827f2afd1a7f87828074b3
texikator <wiktot@mail.ru>
2024-06-07 17:59:40 +0300

    sdf

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -5,7 +5,7 @@ from django.test import Client, TestCase
 
 
 class TaskiAPITestCase(TestCase):
-    def setUP(self):
+    def setUp(self):
         self.guest_client = Client()
 
     def test_list_exists(self):
