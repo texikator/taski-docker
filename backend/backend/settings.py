76ee594ecc5416189ec58a7f210231d24bba8ce2
texikator <wiktot@mail.ru>
2024-06-10 14:27:33 +0300

    changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,11 +4,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY'),
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['tx.zapto.org', '12.0.0.1']
+ALLOWED_HOSTS = ['tx.zapto.org', '127.0.0.1']
 
 
 # Application definition
