2ec578bbb0d6b1cc9fd34432a54cf34725ecd64f
texikator <wiktot@mail.ru>
2024-06-15 18:12:15 +0300

    add default secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
